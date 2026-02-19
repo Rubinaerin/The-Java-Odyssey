44dde4c7022b14ac643e758204cb5bad3e42faf0
Rubina Erin <rerin@stu.okan.edu.tr>
2026-02-19 17:08:30 +0300

    Create Java_Pattern_Syntax_Checker.java

---
+++ Day_09/Java_Pattern_Syntax_Checker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	try {
+              java.util.regex.Pattern.compile(pattern);
+              System.out.println("Valid");
+          } catch (java.util.regex.PatternSyntaxException e) {
+              System.out.println("Invalid");
+          }
+          testCases--;
+		}
+	}
+}
+
+
